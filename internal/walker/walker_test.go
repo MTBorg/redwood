22056fe9dc53dc43bf1884d727303bfe16157061
Martin Terneborg <martinterneborg@protonmail.com>
2026-04-20 22:39:30 +0200

    WIP walker

---
+++ internal/walker/walker_test.go
@@ -0,0 +1,61 @@
+package walker
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkWalker(b *testing.B) {
+	root, err := generateTestDirHierarchy(b, 20, 10000)
+	assert.NoError(b, err)
+	rootFS := os.DirFS(root)
+
+	for b.Loop() {
+		fs.WalkDir(rootFS, ".", func(path string, d fs.DirEntry, err error) error {
+			return nil
+		})
+	}
+	b.ReportMetric(b.Elapsed().Seconds()/float64(b.N), "seconds/op")
+}
+
+// func TestWalker(t *testing.T) {
+// 	fmt.Println("Root directory:", rootDir)
+
+// 	for range 1000 {
+// 		p := generatePath(5)
+// 		p = path.Join(rootDir, p)
+// 		err := os.MkdirAll(p, 0750)
+// 		require.NoError(t, err)
+// 	}
+// 	t.Log("Directories created. Starting walker...")
+
+// 	time.Sleep(1000 * time.Second)
+// }
+
+func generateTestDirHierarchy(t testing.TB, levels int, width int) (string, error) {
+	rootDir := t.TempDir()
+	for range width {
+		p := generatePath(levels)
+		p = path.Join(rootDir, p)
+		err := os.MkdirAll(p, 0750)
+		if err != nil {
+			return "", fmt.Errorf("os.MkdirAll: %w", err)
+		}
+	}
+	return rootDir, nil
+}
+
+func generatePath(levels int) string {
+	components := []string{}
+	for range levels {
+		component := uuid.NewString()[:8]
+		components = append(components, component)
+	}
+	return path.Join(components...)
+}
